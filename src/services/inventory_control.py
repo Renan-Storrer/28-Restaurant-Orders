5aa363842a1b7abc6f42c088d5620e784934ac6a
Renan-Storrer <storrerrenan@gmail.com>
2023-06-26 20:17:18 -0300

    feat: implementa função consume_recipe e check_recipe_availability(req05)

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -10,25 +10,34 @@ Inventory = Dict[Ingredient, int]
 
 
 def read_csv_inventory(inventory_file_path=BASE_INVENTORY) -> Inventory:
-    inventory = dict()
+    inventory = {}
 
     with open(inventory_file_path, encoding="utf-8") as file:
-        for row in DictReader(file):
+        csv_reader = DictReader(file)
+        for row in csv_reader:
             ingredient = Ingredient(row["ingredient"])
             inventory[ingredient] = int(row["initial_amount"])
 
     return inventory
 
 
-# Req 5
 class InventoryMapping:
     def __init__(self, inventory_file_path=BASE_INVENTORY) -> None:
         self.inventory = read_csv_inventory(inventory_file_path)
 
-    # Req 5.1
     def check_recipe_availability(self, recipe: Recipe) -> bool:
-        pass
+        for ingredient in recipe:
+            not_available = ingredient not in self.inventory
+            not_have_qnt = self.inventory.get(ingredient, 0) < int(recipe[ingredient])
 
-    # Req 5.2
+            if not_available or not_have_qnt:
+                return False
+
+        return True
+
     def consume_recipe(self, recipe: Recipe) -> None:
-        pass
+        if not self.check_recipe_availability(recipe):
+            raise ValueError("Recipe ingredients not available in inventory.")
+
+        for ingredient, amount in recipe.items():
+            self.inventory[ingredient] -= int(amount)
