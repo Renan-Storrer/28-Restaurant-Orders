8d5ec76c8dd44252c0d39e6a021dfa7f51c605db
Renan-Storrer <storrerrenan@gmail.com>
2023-06-26 20:53:22 -0300

    feat: implementa função consume_recipe e check_recipe_availability(req05)

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -9,26 +9,35 @@ BASE_INVENTORY = "data/inventory_base_data.csv"
 Inventory = Dict[Ingredient, int]
 
 
-def read_csv_inventory(inventory_file_path=BASE_INVENTORY) -> Dict:
-    inventory = dict()
+def read_csv_inventory(inventory_file_path=BASE_INVENTORY) -> Inventory:
+    inventory = {}
 
     with open(inventory_file_path, encoding="utf-8") as file:
-        for row in DictReader(file):
+        csv_reader = DictReader(file)
+        for row in csv_reader:
             ingredient = Ingredient(row["ingredient"])
             inventory[ingredient] = int(row["initial_amount"])
 
     return inventory
 
 
-# Req 5
 class InventoryMapping:
     def __init__(self, inventory_file_path=BASE_INVENTORY) -> None:
         self.inventory = read_csv_inventory(inventory_file_path)
 
-    # Req 5.1
-    def check_recipe_availability(self, recipe: Recipe):
-        pass
+    def check_recipe_availability(self, recipe: Recipe) -> bool:
+        for ingredient in recipe:
+            not_available = ingredient not in self.inventory
+            not_have_qnt = self.inventory.get(ingredient, 0) < int(recipe[ingredient])
 
-    # Req 5.2
+            if not_available or not_have_qnt:
+                return False
+
+        return True
+
     def consume_recipe(self, recipe: Recipe) -> None:
-        pass
+        if not self.check_recipe_availability(recipe):
+            raise ValueError("Recipe ingredients not available in inventory.")
+
+        for ingredient, amount in recipe.items():
+            self.inventory[ingredient] -= int(amount)
